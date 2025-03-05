3ce567f0c651434f7c6133fc04fd6bb2e689e2f7
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-03-05 04:43:05 +0000

    Added pizza_cost.py with constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created By: Jayden Smith
+# Date: Feb 28, 2025
+# This programs holds the constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.50
---
+++ pizza_cost.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Created By: Jayden Smith
+# Date: Feb 28, 2025
+# This programs calculates the cost of a pizza.
+
+import constants
+
+
+def main():
+    # Asks for user input for the diameter of pizza.
+    pizza_diameter = int(input("Please enter the diameter of your Pizza! (in)"))
+    # Calculates subtotal, tax and total.
+    subtotal = (
+        (pizza_diameter * constants.INGRED_COST)
+        + constants.LABOUR_COST
+        + constants.RENTAL_COST
+    )
+    tax = constants.HST * subtotal
+    total = subtotal + tax
+    # displays total rounded 2 decimals.
+    print("The Cost of your Pizza is ${:.2f}".format(total))
+
+
+if __name__ == "__main__":
+    main()
